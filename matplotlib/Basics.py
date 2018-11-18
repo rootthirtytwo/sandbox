f92ec96f1052dd72649a4d15418d87481f408dff
rootthirtytwo <rootthirtytwo@outlook.com>
2018-11-17 20:05:53 -0500

    basic chart options are added and finalized the basic code.

--- matplotlib/Basics.py
+++ matplotlib/Basics.py
@@ -1,6 +1,97 @@
+# libraries
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 
-print(pd.__version__)
+'''
+Notes
+-----
+**Format Strings**
+
+A format string consists of a part for color, marker and line::
+
+    fmt = '[color][marker][line]'
+
+Each of them is optional. If not provided, the value from the style
+cycle is used. Exception: If ``line`` is given, but no ``marker``,
+the data will be a line without markers.
+
+**Colors**
+
+The following color abbreviations are supported:
+
+=============    ===============================
+character        color
+=============    ===============================
+``'b'``          blue
+``'g'``          green
+``'r'``          red
+``'c'``          cyan
+``'m'``          magenta
+``'y'``          yellow
+``'k'``          black
+``'w'``          white
+=============    ===============================
+
+If the color is the only part of the format string, you can
+additionally use any  `matplotlib.colors` spec, e.g. full names
+(``'green'``) or hex strings (``'#008000'``).
+
+**Markers**
+
+=============    ===============================
+character        description
+=============    ===============================
+``'.'``          point marker
+``','``          pixel marker
+``'o'``          circle marker
+``'v'``          triangle_down marker
+``'^'``          triangle_up marker
+``'<'``          triangle_left marker
+``'>'``          triangle_right marker
+``'1'``          tri_down marker
+``'2'``          tri_up marker
+``'3'``          tri_left marker
+``'4'``          tri_right marker
+``'s'``          square marker
+``'p'``          pentagon marker
+``'*'``          star marker
+``'h'``          hexagon1 marker
+``'H'``          hexagon2 marker
+``'+'``          plus marker
+``'x'``          x marker
+``'D'``          diamond marker
+``'d'``          thin_diamond marker
+``'|'``          vline marker
+``'_'``          hline marker
+=============    ===============================
+
+**Line Styles**
+
+=============    ===============================
+character        description
+=============    ===============================
+``'-'``          solid line style
+``'--'``         dashed line style
+``'-.'``         dash-dot line style
+``':'``          dotted line style
+=============    ===============================
+
+Example format strings::
+
+    'b'    # blue markers with default shape
+    'ro'   # red circles
+    'g-'   # green solid line
+    '--'   # dashed line with default color
+    'k^:'  # black triangle_up markers connected by a dotted line
+'''
+
+# variables
+x = np.linspace(0,5,11)
+y = x ** 2
+
+plt.xlabel('x')
+# plat grapth
+plt.plot(x, y ,'ro--')
+plt.ylabel('y')
+plt.title('Simple Chart')
 
