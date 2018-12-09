e37e4bef68c93ee4294003e8647e74ab0af1bc0e
rootthirtytwo <rootthirtytwo@outlook.com>
2018-12-08 23:11:52 -0500

    added write data to file code

--- yaml/BasicRead.py
+++ yaml/BasicRead.py
@@ -6,17 +6,31 @@ def yaml_loader(file_path):
         data = yaml.load(f)
     return data
 
-def yaml_dump(file_path, data):
+def yaml_dump(data,file_path):
     '''Dump data to file'''
     with open(file_path, "w") as f:
         yaml.dump(data, f)
 
 if __name__ == "__main__":
+
+    # reading from file
     file_path = "basic.yaml"
 
     data = yaml_loader(file_path)
 
     items = data.get("items")
 
+    print(data)
+
     for k, v in items.items():
         print(k)
+
+    # writing to file
+    write_file_path = "basic_write.yaml"
+    write_data = {"items":
+                      {
+                          "buses": 10,
+                          "cars": 20
+                      }}
+
+    yaml_dump(write_data, write_file_path)
