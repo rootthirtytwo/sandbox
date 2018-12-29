6ab2cb00a934d6ffd077afaf8b185d5d4275a353
rootthirtytwo <rootthirtytwo@outlook.com>
2018-12-29 16:07:09 -0500

    initial regular expression basic code check in.

---
+++ regex/Basic.py
@@ -0,0 +1,49 @@
+import re
+
+
+# match : match = re.match(pat, str)
+
+str = 'How ?s How ry the weather today?'
+
+match = re.match(r'How \W', str)
+
+print(match)
+if match:
+    print('String found : {}'.format(match.group()))
+else:
+    print('No Match')
+
+# repetition example
+
+match = re.search(r're+', 'rees') # 'r' followed by 1 or more 'e's
+if match is not None:
+    print(match.group()) # prints ree
+
+match = re.search(r're+s*', 'rees') # 'r' followed by 1 or more 'e's and 0 or more occurrences of 's'
+if match is not None:
+    print(match.group()) # prints rees
+
+match = re.search(r're+s*', 'reeses') # 'r' followed by 1 or more 'e's and 0 or more occurrences of 's'
+if match is not None:
+    print(match.group()) # prints rees
+
+match = re.search(r'^be*', 'bees') # Stats with character 'b' and followed by 0 or more occurrences of 'e'
+if match is not None:
+    print(match.group())
+
+
+email = 'john.reid@company.com' # very basic email id validation
+
+match = re.search(r'[\w.-]+@][\w-]+\.\w+', email)
+if match is None:
+    print('{} is {}'.format(email,'bad email id'))
+else:
+    print('{} is {}'.format(email, 'good email id'))
+
+# explaining group
+match = re.search(r'([\w.-]+)@([\w-]+\.\w+)', email) # grouping blocks are enclosed inside the parenthesis().
+if match is not None:
+    print("Full match string: ", match.group())
+    print("First block match: ", match.group(1))
+    print("Second block match: ",match.group(2))
+
---
+++ regex/Notes.md
@@ -0,0 +1,26 @@
+## Basic Patterns
+* . Any character
+* \w : Alphanumeric characters
+* \W : Non alphanumeric characters
+* \d : Any digits [0-9]
+* \D : Non digits 
+* \b : boundary between word and non-word
+* \s : Any whitespace
+* \S : Any non whitespace characters
+* \t : Tab
+* \n : Newline
+* \r : Return
+* ^  : start of the match
+* $  : end of the match
+* \  : Escape character "specialness" of a character.
+
+## Repetition
+
+* \+ : One or more occurrences
+* \* : Zero or more occurrences
+* \? : Zero or one occurrence
+
+## *References*
+1. Google for Education. [link](https://developers.google.com/edu/python/regular-expressions)
+2. RegexOne. [Link](https://regexone.com/references/python)
+
