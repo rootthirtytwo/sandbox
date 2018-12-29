9c93e767869b12e336d5e97479fa5526b7fb4473
rootthirtytwo <rootthirtytwo@outlook.com>
2018-12-28 23:21:58 -0500

    explained class and static methods

--- OOP/Advanced.py
+++ OOP/Advanced.py
@@ -1,7 +1,10 @@
+import datetime
+
 # parent class
 class Employee():
 
     raise_amt = 1.05
+    num_of_emp = 0
 
     # initializing
     def __init__(self, fname, lname, pay):
@@ -10,14 +13,33 @@ class Employee():
         self.fullname = self.fname.title()+' '+self.lname.title()
         self.email = self.fname+'.'+self.lname+'@company.com'
         self.pay = pay
+        Employee.num_of_emp += 1
 
-    # representation method
+
+    # special method : representation
     def __repr__(self):
         return self.fname.title()+' '+self.lname.title()
 
     # object method
     def raise_pay(self):
         self.pay = ( self.pay * self.raise_amt )
+
+    # class methods
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amt = amount
+
+    @classmethod
+    def from_strig(cls, txt):
+        fname, lname, pay = txt.split('-')
+        return cls(fname, lname, pay)
+
+    # static methods
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
 
 # child class of Emplyee
 class Developer(Employee):
@@ -52,18 +74,25 @@ class Manager(Employee):
 
     # object method to display employee
     def print_employee(self):
+        print("Manger: ", self.fullname)
         for emp in self.emplyees:
-            print('==>', emp.fullname)
+            print('Dev =>', emp.fullname)
+
+# defining objects for Employee class
+emp1 = Employee.from_strig('Ram-Charan-120000')
+print(emp1.fullname)
+print(emp1.email)
+
 
 # defining objects for Developer class
-dev1 = Developer('John', 'Kadam', 80000, 'Python')
+dev1 = Developer('John','Kadam', 80000, 'Python')
 dev2 = Developer('Dev', 'Patel', 75000, 'Python')
 dev3 = Developer('Adam', 'Kurlf', 90000, 'Java')
+dev4 = Developer('Kushal', 'Kaur', 95000, '.Net')
 
 # defining object for Manager class
 mgr01 = Manager('Kumar','Pai', 120000, [])
-print("No developers")
-print(mgr01.fullname)
+print("\nNo developers", sep='\n')
 mgr01.print_employee()
 
 print('\nAfter adding developers', sep='\n')
@@ -75,4 +104,20 @@ mgr01.print_employee()
 print('\nRemoving one developer',sep='\n')
 mgr01.remove_employee(dev2)
 mgr01.print_employee()
+
+print('\nPrints the number of objects created with the class', sep='\n')
+print(mgr01.num_of_emp)
+
+print("\nClass method explained", sep='\n')
+print("Before ", Employee.raise_amt)
+Employee.set_raise_amt(1.20)
+print("After ", Employee.raise_amt)
+print("Developer ", dev1.raise_amt)
+print("Manager ", mgr01.raise_amt)
+
+# testing the static method
+print("\nTesting static method")
+curr_time = datetime.date.today()
+
+print('{0} is working day ? {1}'.format(str(curr_time), Employee.is_workday(curr_time)))
 
