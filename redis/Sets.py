3271acfec40872f5c9808e9f9b50b6a6d8d07570
rootthirtytwo <rootthirtytwo@outlook.com>
2019-01-07 21:25:30 -0500

    added some more function to set

--- redis/Sets.py
+++ redis/Sets.py
@@ -5,9 +5,45 @@ import redis
 r = redis.Redis(host='localhost', port=6379, db=0)
 
 # creating a simple set
-r.sadd('SET001','a','b', 'c')
+r.sadd('ST001','a','b', 'c')
 
 # print the length of the set
-print("Length of the string: ",r.scard('SET001'))
+print("Length of the stri/ng: ",r.scard('ST001'))
 
-print("Set members: ", r.smembers('SET001'))
+# members of the set
+print("Members of ST001 ", r.smembers('ST001'))
+
+# creating second set
+r.sadd('ST002', 'b','c', 'd')
+print("Members of ST002 ", r.smembers('ST001'))
+
+# intersect explained
+print("Set intersect between ST001 and ST002 ", r.sinter('ST001', 'ST002'))
+
+# union explained
+print("Set union between ST001 and ST002 ", r.sunion('ST001', 'ST002'))
+
+# diff explained
+print("Set difference between ST001 and ST002 ", r.sdiff('ST001', 'ST002'))
+
+# ismember explained, if it a member it returns True or else False
+print("'a' is member of ST001  ", r.sismember("ST001", 'a'))
+print("'a' is member of ST002  ", r.sismember("ST002", 'a'))
+
+# moving member from one set to another
+print("Move 'a' from ST001 to ST002..")
+r.smove("ST001", 'ST002', 'a')
+print(r.smembers('ST001'))
+print(r.smembers('ST002'))
+
+# pop one item from set
+r.spop('ST001')
+print("After pop one item from set ST001", r.smembers('ST001'))
+
+# removing specific member from set
+
+r.srem('ST002', 'a')
+print("remove 'a' from set ST002", r.smembers("ST002"))
+
+#Usage of scan
+print(r.sscan("ST002",0,'b', 1))
