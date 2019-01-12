82fd0204c6c270eecd6adb580bcf77034f3e66ba
rootthirtytwo <rootthirtytwo@outlook.com>
2019-01-12 15:02:09 -0500

    Selection sort code

---
+++ search_algorithms/SelectionSearch.py
@@ -0,0 +1,27 @@
+def selection_search(mylist):
+    '''
+
+    :param mylist: unsorted list
+    :return: sorted list
+    '''
+
+    for i in range(len(mylist)):
+
+        first_item = i
+
+        for j in range(i, len(mylist)):
+            if mylist[j] < mylist[first_item]:
+                first_item = j
+        mylist[i],mylist[first_item] = mylist[first_item], mylist[i]
+
+    return mylist
+
+if __name__ == "__main__":
+
+    lst = [2,5,1,3,4]
+
+    print("Umsorted list : ", lst)
+    print("Sorted list :", selection_search(lst))
+
+    # time complexity O(n^2)
+
